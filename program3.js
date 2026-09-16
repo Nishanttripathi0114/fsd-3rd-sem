73e79398a15f77e1e17d2074727c383e32325d2c
nishant <nishnattripathi@gmail.com>
2026-09-17 00:06:03 +0530

    lab1

---
+++ program3.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Server': 'Node.js'
+    });
+
+    res.end('Hello World');
+});
+
+const PORT = 30000;
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
